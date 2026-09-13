2e209a873796203ed2cd43996dc2663df53c00a7
kcantoray <kcantoray@luc.edu>
2026-09-12 20:27:22 -0500

    Update week02.py

--- week02.py
+++ week02.py
@@ -7,12 +7,26 @@ def add(
   """Add a new character record to the underlying list."""
   underlying.append([first_name, last_name, role])
 
-st_characters = [
-    ["Jim", "Hopper", "Chief of Police"],
-    ["Eleven", "", "Psychokinetic Overachiever"],
-    ["Dustin", "Henderson", "Science Enthusiast"]
-]
 
-add("Yuri", "Ismaylov", "Smuggler", st_characters)
+def add_unique_first_name(
+  first_name: str,
+  last_name: str,
+  role: str,
+  underlying: list[list[str]]
+) -> None:
+  """Add a record if the first name is not already in the list."""
+
+  found = False
+  
+  for character in underlying:
+    if character[0] == first_name:
+      found = True
+
+if not found:
+  add(first_name, last_name, role, underlying)
+
+add_unique_first_name("Jim", "Byers", "Test", st_characters)
+add_unique_first_name("Lucas", "Sinclair", "Friend", st_characters)
 
 print(st_characters)
+
