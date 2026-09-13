6e5b0f3e97e5cb09845f8bf23a05308136f8a01f
kcantoray <kcantoray@luc.edu>
2026-09-12 20:19:04 -0500

    Update week02.py

--- week02.py
+++ week02.py
@@ -1,1 +1,18 @@
+def add(
+  first_name: str,
+  last_name: str,
+  role: str,
+  underlying: list[list[str]]
+) -> None:
+  """Add a new character record to the underlying list."""
+  underlying.append([first_name, last_name, role])
 
+st_characters = [
+    ["Jim", "Hopper", "Chief of Police"],
+    ["Eleven", "", "Psychokinetic Overachiever"],
+    ["Dustin", "Henderson", "Science Enthusiast"]
+]
+
+add("Yuri", "Ismaylov", "Smuggler", st_characters)
+
+print(st_characters)
