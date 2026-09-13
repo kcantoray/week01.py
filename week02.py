4d208c09826ff79db4933e6eb0123aa9ab898bd7
kcantoray <kcantoray@luc.edu>
2026-09-12 20:39:13 -0500

    Implement add_unique_last_name function
    Added a function to add records with unique last names.

--- week02.py
+++ week02.py
@@ -25,6 +25,24 @@ def add_unique_first_name(
   if not found:
       add(first_name, last_name, role, underlying)
 
+def add_unique_last_name(
+  first_name: str,
+  last_name: str,
+  role: str,
+  underlying: list[list[str]]
+) -> None:
+   """Add a record if the last name is not already in the list."""
+
+found = False
+
+for character in underlying:
+  if character[1] == last_name:
+    found = True
+
+if not found:
+    add(first_name, last_name, role, underlying)
+  
+
 st_characters = [
     ["Jim", "Hopper", "Chief of Police"],
     ["Eleven", "", "Psychokinetic Overachiever"],
@@ -35,5 +53,9 @@ add("Yuri", "Ismaylov", "Smuggler", st_characters)
 
 add_unique_first_name("Jim", "Byers", "Test", st_characters)
 add_unique_first_name("Lucas", "Sinclair", "Friend", st_characters)
+add_unique_last_name("Will", "Hopper", "Test", st_characters)
+add_unique_last_name("Mike", "Wheeler", "Friend", st_characters)
+
+print(st_characters)
 
 print(st_characters)
