ede1bd13f2c5cebace97cac0a9c78d444ab893c9
kcantoray <kcantoray@luc.edu>
2026-09-12 20:55:39 -0500

    Implement remove_first_name function
    Added a function to remove the first record by first name and updated the main logic to call this function.

--- week02.py
+++ week02.py
@@ -41,8 +41,27 @@ def add_unique_last_name(
 
     if found == False:
         add(first_name, last_name, role, underlying)
-  
 
+def remove_first_name(
+  first_name: str,
+  underlying: list[list[str]]
+) -> list[str] | None:
+  """Remove and return the first record with the given first name."""
+
+  found = Flase
+  index = 0
+  result = None
+
+  for i in range(len(underlying)):
+    if underlying[1][0] == first_name:
+        if found == False:
+            found = True
+            index = i
+
+  if found:
+      result = underlying.pop(index)
+
+  return result
 st_characters = [
     ["Jim", "Hopper", "Chief of Police"],
     ["Eleven", "", "Psychokinetic Overachiever"],
@@ -55,6 +74,9 @@ add_unique_first_name("Jim", "Byers", "Test", st_characters)
 add_unique_first_name("Lucas", "Sinclair", "Friend", st_characters)
 add_unique_last_name("Will", "Hopper", "Test", st_characters)
 add_unique_last_name("Mike", "Wheeler", "Friend", st_characters)
+removed = remove_first_name("Lucas", st_characters)
+print(removed)
+print(st_characters)
 
 print(st_characters)
 
